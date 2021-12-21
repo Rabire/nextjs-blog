2116505b27af6ffedcfec03cbb5ea3f516f7477c
Rabire <rabireh@outlook.fr>
2021-12-21 23:06:59 +0100

    css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,21 +1,13 @@
 import Link from "next/link";
 import Head from "next/head";
-import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
-        <title>First post</title>
+        <title>First Post</title>
       </Head>
-
-      <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyOnload"
-        onLoad={() =>
-          console.log(`script loaded correctly, window.FB has been populated`)
-        }
-      />
 
       <h1>First Post</h1>
       <h2>
@@ -23,6 +15,6 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
